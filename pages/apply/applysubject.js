9903d8a5cc3f77c6c830eb1c4e988194293c10a8
406798106@qq.com <406798106@qq.com>
2017-01-19 17:51:24 +0800

    51报名管家微信小程序

--- pages/apply/applysubject.js
+++ pages/apply/applysubject.js
@@ -3,12 +3,12 @@
 Page({
     data: {
         markers: [{
-            iconPath: "/images/map/addresslocalization.png",
+            iconPath: "/images/map/localization.png",
             id: 0,
             latitude: 23.099994,
             longitude: 113.324520,
-            width: 50,
-            height: 50
+            width: 20,
+            height: 20
         }],
         polyline: [{
             points: [{
@@ -24,12 +24,12 @@ Page({
         }],
         controls: [{
             id: 1,
-            iconPath: '/images/map/addresslocalization.png',
+            iconPath: '/images/map/localization.png',
             position: {
                 left: 0,
                 top: 300 - 50,
-                width: 50,
-                height: 50
+                width: 20,
+                height: 20
             },
             clickable: true
         }]
