7c697f9bf1e76428dc2eba419079e9f76b83e8d1
406798106@qq.com <406798106@qq.com>
2017-01-19 17:30:04 +0800

    51报名管家微信小程序

--- pages/apply/applysubject.js
+++ pages/apply/applysubject.js
@@ -1,19 +1,38 @@
 // pages/apply/applysubject.js
+// map.js
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
-  },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
-  }
+    data: {
+        markers: [{
+            iconPath: "/images/map/addresslocalization.png",
+            id: 0,
+            latitude: 23.099994,
+            longitude: 113.324520,
+            width: 50,
+            height: 50
+        }],
+        polyline: [{
+            points: [{
+                longitude: 113.3245211,
+                latitude: 23.10229
+            }, {
+                longitude: 113.324520,
+                latitude: 23.21229
+            }],
+            color: "#FF0000DD",
+            width: 2,
+            dottedLine: true
+        }],
+        controls: [{
+            id: 1,
+            iconPath: '/images/map/addresslocalization.png',
+            position: {
+                left: 0,
+                top: 300 - 50,
+                width: 50,
+                height: 50
+            },
+            clickable: true
+        }]
+    }
+
 })
