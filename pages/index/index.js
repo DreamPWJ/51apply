1afda0380168e63f10154789c74bc45d3845d15a
406798106@qq.com <406798106@qq.com>
2017-01-20 19:10:15 +0800

    51报名管家微信小程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,15 +3,9 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+
   onLoad: function () {
   }
 })
