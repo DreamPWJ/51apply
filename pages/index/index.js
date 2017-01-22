47aa78f749541cc4c4724bdc22c18a5754a979b8
406798106@qq.com <406798106@qq.com>
2017-01-22 10:28:39 +0800

    51报名管家微信小程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,10 +2,14 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
+    data: {},
 
-  },
-
-  onLoad: function () {
-  }
+    onLoad: function () {
+    },
+    //事件处理函数
+    applysubject: function () {
+        wx.navigateTo({
+            url: '../apply/applysubject'
+        })
+    }
 })
