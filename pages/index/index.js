606eda486ae816afbe83f1a898896cabbfc9de3e
406798106@qq.com <406798106@qq.com>
2017-02-07 17:38:26 +0800

    51报名管家微信小程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,19 +16,13 @@ Page({
             this.getHeadExamType
         )
         //章节练习
-        util.https(app.globalData.api + "/GetExamTestList", "POST", {
-                inputJson: {
-                    ExamTypeId: 1, //考试类别ID，该值必须传入，无类型就传入0
-                    SubjectId: 1, //考试科目ID，该值必须传入
-                    TestTypeId: 3,  //题目类别 1是历年真题，2是模拟试题，3是章节练习，4是每日一练
-                    CurrentPage: 1,
-                    PageSize: 5
-                },
+        util.https(app.globalData.api + "/GetNewestTestTitle", "GET", {
                 praviteKey: 'oiox3tmqu1sn56x7occdd'
             },
             this.getExamTestList
         )
     },
+    //考试报名列表
     getHeadExamType: function (data) {
         console.log(data);
         this.setData({
@@ -36,6 +30,7 @@ Page({
         });
 
     },
+    //章节练习
     getExamTestList: function (data) {
         console.log(data);
         this.setData({
