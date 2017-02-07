128523a5a334cb9324d96508bacb2b7fe10cd3cb
406798106@qq.com <406798106@qq.com>
2017-02-07 17:26:46 +0800

    51报名管家微信小程序

--- pages/apply/applysubject.js
+++ pages/apply/applysubject.js
@@ -3,6 +3,26 @@
 var app = getApp()
 Page({
     data: {
+        array: ['美国', '中国', '巴西', '日本'],
+        objectArray: [
+            {
+                id: 0,
+                name: '美国'
+            },
+            {
+                id: 1,
+                name: '中国'
+            },
+            {
+                id: 2,
+                name: '巴西'
+            },
+            {
+                id: 3,
+                name: '日本'
+            }
+        ],
+        index: 0,
         markers: [{
             iconPath: "/images/map/localization.png",
             id: 0,
@@ -35,10 +55,17 @@ Page({
             clickable: true
         }]
     },
-  //事件处理函数
-  applyperson: function() {
-    wx.navigateTo({
-      url: 'applyperson'
-    })
-  },
+    //事件处理函数
+    applyperson: function () {
+        wx.navigateTo({
+            url: 'applyperson'
+        })
+    },
+    //地区选择
+    bindPickerChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            index: e.detail.value
+        })
+    }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,26 +4,42 @@ var app = getApp();
 var util = require('../../utils/util.js');
 Page({
     data: {
-        headLinesData: "",//考试头条数据
         hiddenLoading: false
     },
 
     onLoad: function () {
-        util.https(app.globalData.api + "/GetExamNotice", "GET", {
-                inputJson: {
-                    CurrPage: 1,
-                    PageSize: 2
-                },
+        //考试报名列表
+        util.https(app.globalData.api + "/GetHeadExamType", "GET", {
                 praviteKey: 'oiox3tmqu1sn56x7occdd'
             }
             ,
-            this.getHeadLines
+            this.getHeadExamType
+        )
+        //章节练习
+        util.https(app.globalData.api + "/GetExamTestList", "POST", {
+                inputJson: {
+                    ExamTypeId: 1, //考试类别ID，该值必须传入，无类型就传入0
+                    SubjectId: 1, //考试科目ID，该值必须传入
+                    TestTypeId: 3,  //题目类别 1是历年真题，2是模拟试题，3是章节练习，4是每日一练
+                    CurrentPage: 1,
+                    PageSize: 5
+                },
+                praviteKey: 'oiox3tmqu1sn56x7occdd'
+            },
+            this.getExamTestList
         )
     },
-    getHeadLines: function (data) {
+    getHeadExamType: function (data) {
         console.log(data);
         this.setData({
-            headLinesData: data.Data
+            headExamType: data.Data
+        });
+
+    },
+    getExamTestList: function (data) {
+        console.log(data);
+        this.setData({
+            examTestList: data.Data
         });
 
     },
