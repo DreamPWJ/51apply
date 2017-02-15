1a086bcb515e405ab53b41b479e2e86259e3509e
406798106@qq.com <406798106@qq.com>
2017-02-15 18:39:19 +0800

    51报名管家微信小程序

--- pages/apply/applyperson.js
+++ pages/apply/applyperson.js
@@ -1,19 +1,30 @@
 // pages/apply/applyperson.js
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
-  },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
-  }
+    data: {
+        credentialsIndex: 0,
+        credentials: ["身份证", "台湾居民往来大陆通行证", "港澳居民往来大陆通行证", "军人证件", "护照"]
+    },
+    onLoad: function (options) {
+        // 页面初始化 options为页面跳转所带来的参数
+    },
+    onReady: function () {
+        // 页面渲染完成
+    },
+    onShow: function () {
+        // 页面显示
+    },
+    onHide: function () {
+        // 页面隐藏
+    },
+    onUnload: function () {
+        // 页面关闭
+    },
+
+    //证件类型选择
+    bindCredentialsPickerChange: function (e) {
+        this.setData({
+            credentialsIndex: e.detail.value
+        })
+    }
+    ,
 })
