f2d43bf036d2106d34490e570c4a3bd7693f543a
406798106@qq.com <406798106@qq.com>
2017-02-20 19:18:27 +0800

    51报名管家微信小程序

--- pages/address/addressmanage.js
+++ pages/address/addressmanage.js
@@ -1,19 +1,25 @@
 // pages/address/addressmanage.js
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
-  },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
-  }
+    data: {},
+    onLoad: function (options) {
+        // 页面初始化 options为页面跳转所带来的参数
+    },
+    onReady: function () {
+        // 页面渲染完成
+    },
+    onShow: function () {
+        // 页面显示
+    },
+    onHide: function () {
+        // 页面隐藏
+    },
+    onUnload: function () {
+        // 页面关闭
+    },
+    //增加地址页面
+    addDress: function () {
+        wx.navigateTo({
+            url: '../address/addaddress'
+        })
+    }
 })
--- pages/apply/applyperson.js
+++ pages/apply/applyperson.js
@@ -12,13 +12,7 @@ Page({
         nationIndex: 0,
         nation: ["汉", "蒙古", "回", "藏", "维吾尔", "苗", "彝", "壮", "布依", "朝鲜", "侗", "瑶", "白", "土家", "哈尼", "哈萨克", "傣", "黎", "其他"],//民族
         professionIndex: 0,
-        profession: [{id: "30", name: "学生"}, {id: "04", name: "事业单位负责人"}, {id: "05", name: "企业负责人"}, {
-            id: "08",
-            name: "军人"
-        }, {id: "09", name: "不便分类的其他从业人员"}, {id: "10", name: "失业（含待业及无业人员）"}, {id: "29", name: "其他专业技术人员"}, {
-            id: "31",
-            name: "行政办公人员"
-        }, {id: "46", name: "医疗卫生辅助服务人员"}, {id: "47", name: "社会服务和居民生活服务人员"}, {id: "49", name: "其他行业"}],
+        profession: ["学生", "事业单位负责人", "企业负责人", "军人", "不便分类的其他从业人员", "失业（含待业及无业人员）", "其他专业技术人员", "行政办公人员", "医疗卫生辅助服务人员", "社会服务和居民生活服务人员", "其他行业"],
         cultureIndex: 0,
         culture: ["大专", "本科", "硕士", "其他"],
         degreeIndex: 0,
@@ -49,6 +43,10 @@ Page({
         )
         //初始化数据
         inputContent["IDType"] = this.data.credentials[0];
+        inputContent["Nation"] = this.data.nation[0];
+        inputContent["Job"] = this.data.profession[0];
+        inputContent["Admssion"] = this.data.entranceDate[0];
+        inputContent["Education"] = this.data.culture[0];
     },
     onReady: function () {
         // 页面渲染完成
@@ -95,6 +93,7 @@ Page({
         this.setData({
             nationIndex: e.detail.value
         })
+        inputContent[e.currentTarget.id] = this.data.nation[e.detail.value]
     },
 
     //时间类型选择
@@ -108,18 +107,21 @@ Page({
         this.setData({
             entranceDate: e.detail.value
         })
+        inputContent[e.currentTarget.id] = e.detail.value
     },
     //职业类型选择
     bindProfessionPickerChange: function (e) {
         this.setData({
             professionIndex: e.detail.value
         })
+        inputContent[e.currentTarget.id] = this.data.profession[e.detail.value]
     },
     //学历选择
     bindCulturePickerChange: function (e) {
         this.setData({
             cultureIndex: e.detail.value
         })
+        inputContent[e.currentTarget.id] = this.data.culture[e.detail.value]
     },
     //学士选择
     bindDegreePickerChange: function (e) {
@@ -148,25 +150,43 @@ Page({
      * 考试报名最终提交数据
      */
     applySubmit: function () {
+        //根据身份证 自动获取生日和性别
+        if (this.data.credentialsIndex == 0) {
+            var iIdNo = "371122198808188339";
+            var sex = "";
+            var tmpStr = "";
+            if (iIdNo.length == 15) {
+                tmpStr = iIdNo.substring(6, 12);
+                tmpStr = "19" + tmpStr;
+                sex = (iIdNo.substring(14, 15)) % 2 == 0 ? 2 : 1
+            }
+            else {
+                tmpStr = iIdNo.substring(6, 14);
+                sex = (iIdNo.substring(16, 17)) % 2 == 0 ? 2 : 1
+            }
+        }
+
+
         var inputJson = {
             OperatorType: 1,// 如果用户没登陆过，就给0，登录过给1 "提交类别" 1表示没有注册过，第一次报名提交，2表示已经快速注册,3 表示立即支付进入
             //  Name: "",  //姓名
             //   SearchPwd: "", //登录密码，如果是已经注册过的用户不需要再次传入密码，直接设置为空
             //  IDType: "", //1表示身份证号
             //   IDCard: "", //证件号,该值必须传入
-            Birthday: 19960101, // 自动获取身份证号中的8位
-            Nation: "",  //民族
-            Job: "学生", //职位
-            TelNum: "13800138000", //手机号
-            QQNumber: "", //qq号
-            University: "深圳大学",//学校
-            Colledge: "计算机学院",//学院
-            Education: "本科", //学历
-            Admssion: "2010",   //入学年份字符串
+            // Birthday: 19960101, // 自动获取身份证号中的8位
+            // Gender:1,//1表示男，2表示女
+            //  Nation: "",  //民族
+            // Job: "学生", //职位
+            // TelNum: "13800138000", //手机号
+            // QQNumber: "", //qq号
+            // University: "深圳大学",//学校
+            // Colledge: "计算机学院",//学院
+            // Education: "本科", //学历
+            //  Admssion: "2010",   //入学年份字符串
             GraduationTime: "2014", //毕业年份，毕业年份不需要填，直接根据入学年份加上学历就算出毕业年份了
-            MajorCode: "计算机技术",  //专业
-            ClassCode: "1班",          //班级
-            Address: "武汉市金融港", //用户当前地址
+            // MajorCode: "计算机技术",  //专业
+            // ClassCode: "1班",          //班级
+            // Address: "武汉市金融港", //用户当前地址
 
             SchoolID: "1",       //考点ID号，该值必须传入
             SubjectId: "4",    //科目ID号，该值必须传入
@@ -176,6 +196,8 @@ Page({
             BookID: "0", //需要的教材ID，如果有多本就用,号分割，没有预定就是0
             IsJoin: "0"  //模拟考试ID，0表示没参加。最多传入一个考试科目ID 勾选了模拟考试就给1，没勾就给0
         };
+        inputJson.Birthday = tmpStr;
+        inputJson.Gender = sex;
         console.log(inputJson);
         //预约考试报名数据提交
         /*    util.https(app.globalData.api + "/SetOrderExamInfo", "GET", {
@@ -186,5 +208,9 @@ Page({
          console.log(data);
          }
          )*/
+        //支付页面
+        wx.navigateTo({
+            url: '../pay/applypay'
+        })
     }
 })
