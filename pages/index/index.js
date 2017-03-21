e49b7414ce26d8263857ea4cd653a459b22aadee
406798106@qq.com <406798106@qq.com>
2017-03-21 21:06:49 +0800

    51报名管家微信小程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,6 +21,16 @@ Page({
             },
             this.getHeadLines
         )
+
+        //首页活动报名列表
+        util.https(app.globalData.api + "/GetHeadActiveList", "GET", {
+                inputJson: {
+                    StudentID: wx.getStorageSync("StudentId") ? wx.getStorageSync("StudentId") : 0//传入用户的ID，如果是没登录状态就给0，如果是已经登录就返回userID.
+                },
+                praviteKey: app.globalData.praviteKey
+            },
+            this.getHeadActiveList
+        )
     },
     //考试报名列表
     getHeadExamType: function (data) {
@@ -39,11 +49,22 @@ Page({
     },
     //考试头条
     getHeadLines: function (data) {
-        console.log(data);
         this.setData({
             headLine: data.Data
         });
 
     },
+    //首页活动报名列表
+    getHeadActiveList: function (data) {
+        console.log(data);
+        var headActiveList = data.Data;
+        for (var index in headActiveList) {
+            //考试时间差
+            headActiveList[index].differdays = Math.floor((new Date(headActiveList[index].EndDate).getTime() - new Date().getTime()) / (24 * 3600 * 1000));
+        }
+        this.setData({
+            headActiveList: headActiveList
+        });
 
+    },
 })
