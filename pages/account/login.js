d0443ceae2f1cfcc97ab414f9a43855d4e2b4771
406798106@qq.com <406798106@qq.com>
2017-03-27 11:34:04 +0800

    51报名管家微信小程序

--- pages/account/login.js
+++ pages/account/login.js
@@ -1,21 +1,104 @@
 // pages/account/login.js
 var app = getApp();
 var util = require('../../utils/util.js');
+import WxValidate from '../../utils/validate'
+var inputContent = {};//输入内容
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
-  },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
-  }
+    data: {},
+    onLoad: function (options) {
+        // 页面初始化 options为页面跳转所带来的参数
+        //验证表单
+        this.WxValidate = new WxValidate({
+                user: {  //验证规则 input name值
+                    required: true,
+                    minlength: 11
+                },
+                password: {
+                    required: true,
+                    minlength: 3
+                },
+
+            },
+            {
+                user: { //提示信息
+                    required: "请填写身份证或者手机号",
+                    minlength: "账户至少输入11个字符"
+                },
+                password: { //提示信息
+                    required: "请填写密码",
+                    minlength: "账户至少输入3个字符"
+                }
+            })
+    },
+    onReady: function () {
+        // 页面渲染完成
+    },
+    onShow: function () {
+        // 页面显示
+    },
+    onHide: function () {
+        // 页面隐藏
+    },
+    onUnload: function () {
+        // 页面关闭
+    },
+    //获取用户输入
+    bindChange: function (e) {
+        inputContent[e.currentTarget.id] = e.detail.value
+        console.log(inputContent);
+    },
+    loginSubmit: function (e) {//登录
+        //调用验证表单方法
+        const params = e.detail.value
+        console.log(params);
+        if (!this.WxValidate.checkForm(e)) {
+            const error = this.WxValidate.errorList
+            wx.showModal({
+                title: '友情提示',
+                content: error[0].msg,
+                showCancel: false,
+                confirmColor: "#f26604",
+                success: function (res) {
+                    if (res.confirm) {
+                        console.log('用户点击确定');
+                    }
+                }
+            })
+            console.log(error)
+
+            return false
+        }
+        //首页活动报名列表
+        util.https(app.globalData.api + "/UserLogin", "GET", {
+                inputJson: {
+                    IDCard: inputContent.user,	//身份证号
+                    TelNum: inputContent.user,		//手机号码
+                    SearchPwd: inputContent.password,		//密码
+                    StuLatitude: wx.getStorageSync("latitude"), //用户的纬度
+                    StuLongitude: wx.getStorageSync("longitude"), //用户的经度
+                    TypeId: inputContent.user.length > 11 ? 1 : 2,	//注册类型  1 表示身份证号，2表示手机号
+                },
+                praviteKey: app.globalData.praviteKey
+            },
+            this.userLogin
+        )
+    },
+    userLogin: function (data) { //用户登录回调
+        console.log(data);
+        this.setData({
+            userData: data.Data
+        });
+        if (data.StatusCode == 0) {
+            wx.setStorageSync("userData", data.Data);//用户信息
+            wx.showToast({
+                title: '登录成功',
+                icon: 'success',
+                duration: 1000
+            })
+            wx.switchTab({
+                url: '/pages/account/account'
+            })
+
+        }
+    }
 })
